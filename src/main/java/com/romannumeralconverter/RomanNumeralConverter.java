984a591577fa45b7606576b97ce24dda568fa7f6
Jesse Boulard <jesseboulard@gmail.com>
2017-07-21 17:28:12 -0400

    tested if romanNumeralSymbolFor method returns the Roman numeral equivalent of any respective digit

--- src/main/java/com/romannumeralconverter/RomanNumeralConverter.java
+++ src/main/java/com/romannumeralconverter/RomanNumeralConverter.java
@@ -1,9 +1,28 @@
 package com.romannumeralconverter;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumeralConverter {
 
+	private static final Map<Integer, String> CONVERT_TO_ROMAN_NUMERAL_SYMBOL = createMapOfArabicToRomanNumerals();
+	
+	private static Map<Integer, String> createMapOfArabicToRomanNumerals() 
+	{
+		Map<Integer, String> convertToRomanNumeral = new HashMap<Integer, String>();
+		convertToRomanNumeral.put(1, "I");
+		convertToRomanNumeral.put(5, "V");
+		convertToRomanNumeral.put(10, "X");
+		convertToRomanNumeral.put(50, "L");
+		convertToRomanNumeral.put(100, "C");
+		convertToRomanNumeral.put(500, "D");
+		convertToRomanNumeral.put(1000, "M");
+		return Collections.unmodifiableMap(convertToRomanNumeral);
+	}
+	
 	protected String romanNumeralSymbolFor(int arabicNumeral) {
-		return "I";
+		return CONVERT_TO_ROMAN_NUMERAL_SYMBOL.get(arabicNumeral);
 	}
 
 }
--- src/test/java/com/romannumeralconverter/RomanNumeralConverterTest.java
+++ src/test/java/com/romannumeralconverter/RomanNumeralConverterTest.java
@@ -7,10 +7,28 @@ import org.junit.Test;
 
 public class RomanNumeralConverterTest {
 
+	RomanNumeralConverter romanNumeralConverter;
+
+	@Before
+	public void setUp() throws Exception 
+	{
+		romanNumeralConverter = new RomanNumeralConverter();
+	}
+
 	@Test
-	public void ifRomanNumeralSymbolForIsPassedA1ReturnI() {
-		RomanNumeralConverter romanNumeralConverter = new RomanNumeralConverter();
+	public void ifRomanNumeralSymbolForIsPassedA1ReturnI() 
+	{
 		assertEquals("I", romanNumeralConverter.romanNumeralSymbolFor(1));
 	}
 
+	@Test
+	public void ifRomanNumeralSymbolForIsPassedADigitReturnItsRomanNumeralEquivalent() 
+	{
+		assertEquals("V", romanNumeralConverter.romanNumeralSymbolFor(5));
+		assertEquals("X", romanNumeralConverter.romanNumeralSymbolFor(10));
+		assertEquals("L", romanNumeralConverter.romanNumeralSymbolFor(50));
+		assertEquals("C", romanNumeralConverter.romanNumeralSymbolFor(100));
+		assertEquals("D", romanNumeralConverter.romanNumeralSymbolFor(500));
+		assertEquals("M", romanNumeralConverter.romanNumeralSymbolFor(1000));
+	}
 }
