5a86a62be42d9f7b91f9674e5c718c09e2b79f9d
Jesse Boulard <jesseboulard@gmail.com>
2017-07-23 10:55:56 -0400

    tested valueOf method to return correct value when passed any Roman numeral symbol

--- src/main/java/com/romannumeralconverter/RomanNumeralConverter.java
+++ src/main/java/com/romannumeralconverter/RomanNumeralConverter.java
@@ -7,8 +7,22 @@ import java.util.Scanner;
 
 public class RomanNumeralConverter {
 	
+	private static final Map<String, Integer> CONVERT_TO_ARABIC_DIGIT = createMapOfRomanToArabicNumerals();
+	private static Map<String, Integer> createMapOfRomanToArabicNumerals() 
+	{
+		Map<String, Integer> convertToArabicNumeral = new HashMap<String, Integer>();
+		convertToArabicNumeral.put("I", 1);
+		convertToArabicNumeral.put("V", 5);
+		convertToArabicNumeral.put("X", 10);
+		convertToArabicNumeral.put("L", 50);
+		convertToArabicNumeral.put("C", 100);
+		convertToArabicNumeral.put("D", 500);
+		convertToArabicNumeral.put("M", 1000);
+		return Collections.unmodifiableMap(convertToArabicNumeral);
+	}
+
 	public int valueOf(String romanNumeralSymbol) {
-		return 1;
+		return CONVERT_TO_ARABIC_DIGIT.get(romanNumeralSymbol);
 	}
 
 }
--- src/test/java/com/romannumeralconverter/RomanNumeralConverterTest.java
+++ src/test/java/com/romannumeralconverter/RomanNumeralConverterTest.java
@@ -19,4 +19,14 @@ public class RomanNumeralConverterTest {
 	public void ifValueOfIsPassedAnIReturn1() {
 		assertEquals(1, romanNumeralConverter.valueOf("I"));
 	}
+	
+	@Test
+	public void ifValueOfMethodIsPassedAnyRomanNumeralReturnItsValue() {
+		assertEquals(5, romanNumeralConverter.valueOf("V"));
+		assertEquals(10, romanNumeralConverter.valueOf("X"));
+		assertEquals(50, romanNumeralConverter.valueOf("L"));
+		assertEquals(100, romanNumeralConverter.valueOf("C"));
+		assertEquals(500, romanNumeralConverter.valueOf("D"));
+		assertEquals(1000, romanNumeralConverter.valueOf("M"));
+	}	
 }
